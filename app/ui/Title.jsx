31a61e1f447f01f3372d0c6477b76722f33b768a
apktntaj <maiaelarsya@gmail.com>
2024-11-17 16:32:56 +0700

    Revisi redaksi deskripsi.

--- app/ui/Title.jsx
+++ app/ui/Title.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Title({ title }) {
+export default function Title() {
   return (
     <div>
       <h1 className='text-4xl font-extrabold drop-shadow-md tracking-wider text-gray-600 text-center mb-6'>
@@ -8,12 +8,12 @@ export default function Title({ title }) {
       </h1>
       <div className='text-center text-gray-600 text-lg'>
         <p>
-          Solusi otomatis untuk mengambil data pajak dan HS Code dari website
-          INSW secara cepat dan efisien.
+          Solusi otomatis untuk mengambil data pajak situs INSW berdasarkan
+          kumpulan HS Code dari Invoice.
         </p>
         <p className='mb-5'>
-          Dengan INSScan, Anda bisa memproses banyak data sekaligus dan
-          mendapatkan hasil dalam format Excel.
+          Anda cukup masukkan file invoice, lalu kami akan mencari data pajak
+          jika ada item HS Code yang terdeteksi oleh AI kami.
         </p>
       </div>
     </div>

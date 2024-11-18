08f255b40ecdba76796c3d72127a5eee6b274307
apktntaj <maiaelarsya@gmail.com>
2024-11-18 13:50:17 +0700

    mobile input constraint

--- app/ui/Input.jsx
+++ app/ui/Input.jsx
@@ -15,7 +15,7 @@ export default function Input({ setHsCodes }) {
   return (
     <input
       type='file'
-      accept='.xlsx, .xls'
+      accept='.xls,.xlsx,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
       onChange={handleChange}
       className='file-input file-input-bordered w-full lg:max-w-xs max-w-sm'
     />
